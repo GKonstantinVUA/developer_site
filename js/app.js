90c453b95fd459cb66ada3f714cc3aabbeb7717c
KonstantinGV <konstantingua@yahoo.com>
2024-05-26 01:01:05 +0300

    change_26.05.01:00

--- js/app.js
+++ js/app.js
@@ -3311,6 +3311,43 @@
         window.addEventListener("resize", toggleItems);
  //!Викликаємо перевірку вікна при зміні розміру вікна
         }));
+    function findVideos() {
+        let videos = document.querySelectorAll(".testimonial__inner-video");
+        for (let i = 0; i < videos.length; i++) setupVideo(videos[i]);
+    }
+    function setupVideo(video) {
+        let link = video.querySelector(".testimonial__video-link");
+        let media = video.querySelector(".testimonial__video-slide");
+        let button = video.querySelector(".testimonial__play-video");
+        let id = parseMediaURL(media);
+        video.addEventListener("click", (() => {
+            let iframe = createIframe(id);
+            link.remove();
+            button.remove();
+            video.appendChild(iframe);
+        }));
+        link.removeAttribute("href");
+        video.classList.add("testimonial__inner-video--enabled");
+    }
+    function parseMediaURL(media) {
+        let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.(jpg|webp)/i;
+        let url = media.src;
+        let match = url.match(regexp);
+        return match[1];
+    }
+    function createIframe(id) {
+        let iframe = document.createElement("iframe");
+        iframe.setAttribute("allowfullscreen", "");
+        iframe.setAttribute("allow", "autoplay");
+        iframe.setAttribute("src", generateURL(id));
+        iframe.classList.add("testimonial__video-slide");
+        return iframe;
+    }
+    function generateURL(id) {
+        let query = "?rel=0&showinfo=0&autoplay=1";
+        return "https://www.youtube.com/embed/" + id + query;
+    }
+    findVideos();
     const btnUp = {
         el: document.querySelector(".scroll-up"),
         show() {
